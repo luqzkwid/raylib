14c7685e5f42e1cf443f8118ba352e00c5f5f179
luqzkwid <brimmloloo@gmail.com>
2026-03-31 10:14:15 -0300

    Shotgun: Implementing enemies (just rendering them)

--- shotgun/single_click_shoot.cpp
+++ shotgun/single_click_shoot.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+
 #include "raylib-cpp.hpp"
 #include <vector>
+#include <random>
 
 #define WIDTH 600
 #define HEIGHT 400
+#define NUMBER_OF_ENEMIES 20
 typedef struct {
   Vector2 position, velocity;
   float radius;
@@ -15,29 +19,71 @@ typedef struct {
   Vector2 position,velocity;
   float radius; bool active;
 } Bullet;
+typedef struct {
+  Vector2 position,velocity;
+  float radius; int health;
+} Enemy;
 // Globals
 Vector2 p_pos={200,200}, p_vel={5,5};
-// Functions Definitions
+std::vector<Enemy> EnemiesList;
 std::vector<Bullet> BulletList;
+// Functions Definitions
 void shoot(Gun* gun);
 void UpdateAmmo();
 void Draw(Gun* gun, Player* px);
 void AddMovement(Player* px, Gun* gun);
+void AddEnemies();
 
 int main(void){
   raylib::Window window(WIDTH, HEIGHT, "Shotgun Game");
   Player p = {p_pos, p_vel, 20}; // position, velocity, radius
   Gun gun  = {{p_pos.x+10, p_pos.y}, {50, 5}, 0.0f};
+  int rounds = 5;
+  AddEnemies();
   SetTargetFPS(60);
   while(!window.ShouldClose()){
+    shoot(&gun);
     BeginDrawing();
     window.ClearBackground(BLACK);
-    Draw(&gun, &p); 
-      shoot(&gun);
-      AddMovement(&p, &gun);
+      Draw(&gun, &p); 
       UpdateAmmo();
-      
-    EndDrawing();    
+      for(int r=0; r<rounds; r++){
+	for(int i=0; i< EnemiesList.size(); i++){
+	  if(EnemiesList.at(i).position.y + EnemiesList.at(i).radius > HEIGHT){EnemiesList.at(i).position.y = 0;}
+	  if(EnemiesList.at(i).position.x + EnemiesList.at(i).radius > WIDTH ||
+	     EnemiesList.at(i).position.y + EnemiesList.at(i).radius > HEIGHT){
+	    continue;
+	  } else {
+	    DrawCircleV(EnemiesList.at(i).position, EnemiesList.at(i).radius, YELLOW);
+	    EnemiesList.at(i).position.y += EnemiesList.at(i).velocity.y * 0.2;
+	    std::cout << "Enemy: " << i << "\n";
+	    std::cout << "X: " << EnemiesList.at(i).position.x << "\n";
+	    std::cout << "Y: " << EnemiesList.at(i).position.y << "\n";
+	  }
+	}
+      }
+    EndDrawing();
+    AddMovement(&p, &gun);
+  }
+}
+void AddEnemies(){
+  std::random_device rd; //         Rendering random numbers.
+  std::mt19937 gen(rd()); //
+  for(int i=0; i < NUMBER_OF_ENEMIES; i++){
+    std::uniform_int_distribution<> distrib_velocity_x_above(0, 0);
+    std::uniform_int_distribution<> distrib_velocity_y_above(1, 1); 
+    std::uniform_int_distribution<> distrib_position_y_above(-100, -10); 
+    std::uniform_int_distribution<> distrib_position_x_above(0, WIDTH);
+
+    Enemy enemy_above;
+    enemy_above.position.x = (float)distrib_position_x_above(gen);
+    enemy_above.position.y = (float)distrib_position_y_above(gen);
+    enemy_above.velocity.x = (float)distrib_velocity_x_above(gen);
+    enemy_above.velocity.y = (float)distrib_velocity_y_above(gen);
+    enemy_above.radius     = 10;
+    enemy_above.health     = 100;
+
+    EnemiesList.push_back(enemy_above);
   }
 }
 void AddMovement(Player* px, Gun* gun) {
