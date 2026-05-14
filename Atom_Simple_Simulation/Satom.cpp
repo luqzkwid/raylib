aeeb1143b73c7b02a2574fad8b8c7c4cb6783d29
luqzkwid <brimmloloo@gmail.com>
2026-05-14 20:54:55 -0300

    Implementing a more convinient way to see the atom.

--- Atom_Simple_Simulation/Satom.cpp
+++ Atom_Simple_Simulation/Satom.cpp
@@ -1,12 +1,8 @@
 #include <print>
-#include <sstream>
 #include <raylib.h>
 #include <cmath>
-#include "lof.hpp"
-#include <string>
 #include <vector>
 #include <map>
-#include <cassert>
 /***
 **WARNING (for emacs users): for read this, consider disabling the whitespace-mode if it is enabled (Or set to not mark tabs for better reading). And be sure to compile this program with c++23 **
 
@@ -35,13 +31,11 @@ using std::string;
 #define WIDTH  850
 #define HEIGHT 500
 #define ANGLE_FACTOR 360
-Logger lof{Logger::LogLevelError};
 class Atom {
- private:
-    float angle;
-    float radius;
-    int num_of_electrons;
-     map<int, int> layers;
+private:
+    float angle, radius, atomic_radius;
+    int num_of_electrons, electrons_temp;
+    map<int, int> layers;
     map<string, vector<int>> layers_diagnostic;
 
     map<string, vector<int>>
@@ -50,22 +44,26 @@ class Atom {
         lmap["completed_layers"];
         lmap["incompleted_layer"];
         for(auto& [level, max_electrons] : this->layers){
-            if(this->num_of_electrons - max_electrons >= 0){
+            if(this->electrons_temp - max_electrons >= 0){
                 // Here we have a completed layer!
-                this->num_of_electrons -= max_electrons;
+                this->electrons_temp -= max_electrons;
                 lmap["completed_layers"].push_back(level);
             } else {
                 lmap["incompleted_layer"].push_back(level);
                 break;
             }
         }
-        lof.info("Printing the completed layer vector!");
+        print("INFO: Printing the completed layer vector!\n");
         for(int i=0; i<lmap["completed_layers"].size(); i++){
-            print("Elements: {}\n", lmap["completed_layers"].at(i));
+            print("Layers: {} --- MaxElec.: {}\n",
+                  lmap["completed_layers"].at(i),
+                  this->layers[lmap["completed_layers"].at(i)]);
         }
-        lof.info("Printing the incompleted layer vector!");
+        print("INFO: Printing the incompleted layer vector!\n");
         for(int i=0; i<lmap["incompleted_layer"].size(); i++){
-            print("Elements: {}\n", lmap["incompleted_layer"].at(i));
+            print("Layers: {} -- MaxElec.: {}\n",
+                  lmap["incompleted_layer"].at(i),
+                  this->layers[lmap["incompleted_layer"].at(i)]);
         }
         return lmap;
     }
@@ -75,6 +73,7 @@ public:
     Atom(int eletrons, float radius, Vector2 center){
         this->center           = center;
         this->num_of_electrons = eletrons;
+        this->electrons_temp   = eletrons;
         this->radius           = radius;
         this->v_a              = 0.03f;
         this->angle            = 0;
@@ -87,56 +86,84 @@ public:
         vector<int> incomp_layer = layers_diagnostic["incompleted_layer"];
         vector<int> comp_layers = layers_diagnostic["completed_layers"];
 
-        for(int i=1; i <= comp_layers.size() + incomp_layer.size(); i++) {
+        for(int i=1; i <= comp_layers.size(); i++) {
             int electrons_count = this->layers[i];
             float space_electron = 20.0f;
             DrawCircleLinesV(this->center, this->radius+k, RAYWHITE);
-            if(i == incomp_layer.at(0)){ DrawCircleLinesV(this->center, this->radius+k, RED); } //--
             for(int j=1; j <= electrons_count; j++){
                 Vector2 pos = {
                     this->center.x + cosf(this->angle + space_electron) * (this->radius+k),
                     this->center.y + sinf(this->angle + space_electron) * (this->radius+k)
                 };
                 DrawCircleV(pos, 8, BLUE);
-                if(i == incomp_layer.at(0)){DrawCircleV(pos, 8, YELLOW);}
                 space_electron +=10;
             }
+            if(i == comp_layers.size()){
+                electrons_count = this->electrons_temp;
+                const char* electrons = TextFormat("Eletrons: %d", this->electrons_temp);
+                DrawText(electrons, this->center.x + this->radius, this->center.y - this->radius, 24, WHITE);
+                DrawCircleLinesV(this->center, this->radius+k+40, DARKBLUE);
+                for(int a=1; a <= electrons_count; a++){
+                    Vector2 pos = {
+                        this->center.x + cosf(this->angle + space_electron) * (this->radius+k+40),
+                        this->center.y + sinf(this->angle + space_electron) * (this->radius+k+40)
+                    };
+                    DrawCircleV(pos, 8, BLUE);
+                    space_electron+=10;
+                    DrawLineV(this->center, pos, WHITE);
+                    if(a == electrons_count){
+                        float dx, dy;
+                        dx = this->center.x - pos.x;
+                        dy = this->center.y - pos.y;
+                        this->atomic_radius = sqrt((dx*dx) + (dy*dy));
+                    }
+                }
+            }
             this->angle += this->v_a * GetFrameTime();
-            k+=40;
+            k+=this->radius+10;
         }
-    }
-    void hints(){
-        const char* hint = "Press the W key to accelerate the first ATOM!\n";
-        // DrawText(hint, int posX, int posY, int fontSize, Color color);
 
     }
-};
+    void hints(){
+        DrawText("Valencia Layer",
+                 this->center.x + this->atomic_radius - 70,
+                 this->center.y - this->atomic_radius, 24, DARKBLUE);
+
+    };  // HINT
+
+    void enable_controls(){
+        if(IsKeyPressed(KEY_D)){
+            if(this->radius >= 10){
+                this->radius -= 2;
+            };
+        } else if(IsKeyPressed(KEY_I)){
+            if(this->radius <= 40){
+                this->radius += 2;
+            };
+        }
+    }
+}; // CLASS atom
 int
 main(void){
-    Vector2 center = {WIDTH/2,HEIGHT/2};
-    Atom atom_test = Atom{20, 30, center};
-    Atom atom_test_1 = Atom{15, 20, center};
+    Vector2 center = {WIDTH/2,HEIGHT/2 };
+    Atom atom_test = Atom{30, 30, center};
     SetTargetFPS(60);
     InitWindow(WIDTH, HEIGHT, "Atom Simulations");
     while(!WindowShouldClose()){
         if(IsKeyPressed(KEY_S)){atom_test.v_a   +=   0.02f;}
         if(IsKeyPressed(KEY_D)){atom_test.v_a   -= 0.02f;}
 
-        if(IsKeyPressed(KEY_J)){atom_test_1.v_a += 0.02f;}
-        if(IsKeyPressed(KEY_K)){atom_test_1.v_a -= 0.02f;}
+        // if(IsKeyPressed(KEY_J)){atom_test_1.v_a += 0.02f;}
+        // if(IsKeyPressed(KEY_K)){atom_test_1.v_a -= 0.02f;}
 
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){atom_test.center = GetMousePosition();}
-        if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){atom_test_1.center = GetMousePosition();}
+        // if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){atom_test_1.center = GetMousePosition();}
         BeginDrawing();
-        DrawText("Press S/D to increase/decrease the Velocity of the FIRST Atom.",
-        20, 30, 24, WHITE
-        );
-        DrawText("Press J/K to increase/decrease the Velocity of the SECOND Atom.",
-        20, 30+24, 24, WHITE);
 
         ClearBackground(BLACK);
         	atom_test.draw();
-        	atom_test_1.draw();
+        	atom_test.hints();
+        	atom_test.enable_controls();
         EndDrawing();
     }
     //atom_test.hints();
